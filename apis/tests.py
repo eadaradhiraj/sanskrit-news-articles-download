f849fcded44640c00ffb226d225a9f8238e5212e
eadaradhiraj <eadaradhiraj@gmail.com>
2022-11-26 02:43:16 +0530

    ENH: Test case modifications

--- apis/tests.py
+++ apis/tests.py
@@ -1,4 +1,5 @@
 import pytest
+from apis.models import NewsArticlePublishTime
 
 
 @pytest.mark.django_db(True)
@@ -15,6 +16,12 @@ class TestApiModel():
             '/apis/save-samprativartah-articles'
         )
         assert len(mailoutbox) > 1
+        assert hasattr(
+            NewsArticlePublishTime.objects.first(),
+            "timestamp"
+        )
+        assert NewsArticlePublishTime.objects.first().source ==\
+            "SampratiVartah"
         assert response.status_code == 200
 
     def test_samprati_lit_articles(self, client, mailoutbox):
@@ -22,4 +29,10 @@ class TestApiModel():
             '/apis/save-samprativartah-lit-articles'
         )
         assert len(mailoutbox) > 1
+        assert hasattr(
+            NewsArticlePublishTime.objects.first(),
+            "timestamp"
+        )
+        assert NewsArticlePublishTime.objects.first().source ==\
+            "SampratiVartahLiterature"
         assert response.status_code == 200
